3261bcf9148c44d21fcef219c5698bb86ccbb37e
Yassmin-fadloun <88232979+Yassmin-fadloun@users.noreply.github.com>
2025-07-29 02:59:24 +0300

    Update main.tf

--- Terraform/main/main.tf
+++ Terraform/main/main.tf
@@ -1,7 +1,13 @@
 module "network" {
-  source = "../modules/network"
+  source            = "../modules/network"
+  vpc_cidr          = var.vpc_cidr
+  public_subnet_cidr = var.public_subnet_cidr
 }
 
 module "server" {
-  source = "../modules/server"
+  source         = "../modules/server"
+  ami_id         = var.ami_id
+  instance_type  = var.instance_type
+  subnet_id      = module.network.public_subnet_id
+  vpc_id         = module.network.vpc_id
 }
