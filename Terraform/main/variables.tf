e0d3c88f4149ad3f15bcf4a0fa6dd4c7c0cd27ed
Yassmin-fadloun <88232979+Yassmin-fadloun@users.noreply.github.com>
2025-07-29 02:57:23 +0300

    Update variables.tf

--- Terraform/main/variables.tf
+++ Terraform/main/variables.tf
@@ -1,5 +1,15 @@
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "ami_id" {
+  default = "ami-0c7217cdde317cfec"
+}
+
 variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t3.small"
+  default = "t3.small"
 }
