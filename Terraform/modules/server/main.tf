a282d81d0a61ceb820a1b634303f6c925c465730
Yassmin-fadloun <88232979+Yassmin-fadloun@users.noreply.github.com>
2025-07-29 03:02:46 +0300

    Update main.tf

--- Terraform/modules/server/main.tf
+++ Terraform/modules/server/main.tf
@@ -1,10 +1,37 @@
-resource "aws_instance" "jenkins" {
-  ami                    = "ami-04b70fa74e45c3917"  # Amazon Linux 2023 (verify region)
-  instance_type          = var.instance_type
-  subnet_id              = module.network.subnet_id
-  associate_public_ip_address = true
+resource "aws_security_group" "jenkins_sg" {
+  name        = "jenkins-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = var.vpc_id
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "jenkins" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+  associate_public_ip_address = true
   tags = {
-    Name = "Jenkins-Server"
+    Name = "JenkinsInstance"
   }
 }
