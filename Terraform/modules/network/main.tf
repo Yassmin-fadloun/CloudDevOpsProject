2da069c29581fcdb8b7a3dc726042205b5f8c42a
Yassmin-fadloun <88232979+Yassmin-fadloun@users.noreply.github.com>
2025-07-29 03:00:51 +0300

    Update main.tf

--- Terraform/modules/network/main.tf
+++ Terraform/modules/network/main.tf
@@ -1,13 +1,50 @@
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr
 }
 
 resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_cidr
   map_public_ip_on_launch = true
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_network_acl" "public_acl" {
+  vpc_id = aws_vpc.main.id
+  subnet_ids = [aws_subnet.public.id]
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 22
+    to_port    = 22
+  }
+
+  egress {
+    protocol   = "-1"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
 }
